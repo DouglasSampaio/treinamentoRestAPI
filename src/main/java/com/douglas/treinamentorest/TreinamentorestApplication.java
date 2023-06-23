26fb8eab50845f5e902847905c6f352562340840
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-06-23 19:58:13 -0300

    projeto criado

---
+++ src/main/java/com/douglas/treinamentorest/TreinamentorestApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.treinamentorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/treinamentorest/TreinamentorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.treinamentorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
